084e04be5ef5cc0648929520cbcf7c561eba1504
Tsofia Touito <148336183+TsofiaTouito@users.noreply.github.com>
2024-10-21 16:46:58 +0300

    Create MaxSubarray_b.cpp

---
+++ Q5/MaxSubarray_b.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono> 
+
+using namespace std;
+
+//Generate a random number in the range [-25, 74]
+int randGen(int min , int max ){
+    return min + (rand() % (max - min + 1));
+}
+
+
+
+void genRandArray(int arr[], int size){
+
+    for(int i = 0 ; i < size ; i++){
+        arr[i] = randGen(-25, 75);
+    }
+}
+
+
+
+// Time complexity of the algorithm is : O(n^2)
+int MaxSubArray(int arr[], int size){
+
+    int max_sum = 0;
+    for(int i = 0 ; i < size ; i++){
+        int sum = 0;
+        
+        for(int j = i ; j < size ; j++){
+            
+            sum += arr[j];  //sum the current sub-array
+            max_sum = max(sum, max_sum);
+        }
+    }
+
+    return max_sum;
+}
+
+
+
+void printArray(int arr[], int size){
+
+    cout << "[";
+    
+    for(int i = 0 ; i < size ; i++){
+        cout << arr[i] ;
+        
+        if(i < size-1){
+            cout<< ",";
+        }
+    }
+
+    cout << "]" << endl;;
+}
+
+
+
+
+int main(int args, char* argv[]){
+
+    if(args !=  3){
+        cerr << "Usage :" << argv[0] << " seed | input size \n" ;
+        return 1;
+    }
+
+    int seed = atoi(argv[1]);
+    int size = atoi(argv[2]);
+
+
+    if(size <= 0 ){
+        cerr << "The input size must be positive " ;
+        return 1;
+    }
+
+    // Initialize the random number generator with the seed
+    srand(seed);  
+
+
+    int arr[size];
+    genRandArray(arr, size);
+    
+
+    cout <<"The values of the calculated array :" << endl;
+    printArray(arr, size);
+
+
+    int max_sum = MaxSubArray(arr, size);
+    cout << "Max sub-array sum is : " << max_sum << endl;
+
+
+return 0;
+}
